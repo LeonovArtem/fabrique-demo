c0379b36e24970a7f4b1d751e8f702aeb4a75634
Artem Leonov <a.leonov@dats.team>
2022-02-10 20:54:54 +0300

    init

---
+++ src/main/java/com/fabrique/fabriquedemo/FabriqueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fabrique.fabriquedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabriqueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabriqueDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fabrique/fabriquedemo/FabriqueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrique.fabriquedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabriqueDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
